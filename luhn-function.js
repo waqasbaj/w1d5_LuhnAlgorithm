60bf1e1e38cb48697a177789767c4172082c3c63
Waqas Bajwa <waqasbaj@gmail.com>
2018-01-19 17:17:13 +0000

    Initial Commit

---
+++ luhn-function.js
@@ -0,0 +1,28 @@
+
+
+function check(number)
+{
+
+
+
+
+
+}
+
+function toArray(number){
+
+  console.log(" I turn " + number + " Into an array");
+  return array;
+}
+
+function doubleValue(array){
+
+  console.log(" I turn from the right most digit of  " + array + " the double value of the data")
+  return array;
+}
+
+function higherThanNine(array){
+  console.log(" I will if greater than ")
+}
+
+check(12345);
